d6cab4664eb0198db18e46dc04244f95134c8547
niihash <50837663+niihash@users.noreply.github.com>
2026-03-20 00:46:00 -0300

    feat: Add api config for SPA

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'register']);
+Route::get('/logout', [AuthController::class, 'logout']);
