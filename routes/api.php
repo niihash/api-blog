6b96a4da07362492587009abb2c9dda156b390be
niihash <50837663+niihash@users.noreply.github.com>
2026-03-20 01:52:46 -0300

    feat: Add public articles endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PublicArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/articles', [PublicArticleController::class, 'index']);
+Route::get('/articles/{article}', [PublicArticleController::class, 'show']);
 
 Route::middleware('auth_sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
