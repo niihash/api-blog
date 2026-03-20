5cf29900e81c95126394c3a2f27bfa3808e59804
niihash <50837663+niihash@users.noreply.github.com>
2026-03-20 01:28:34 -0300

    feat: Add articles endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth_sanctum')->group(function () {
+    Route::apiResource('/articles', ArticleController::class);
+});
